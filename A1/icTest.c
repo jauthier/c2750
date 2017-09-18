cffdd61c72a865f8bf323ebaaf009c5eb40ba87f
jauthier <jauthier@mail.uoguelph.ca>
2017-09-18 13:22:22 -0400

    a1

--- A1/icTest.c
+++ A1/icTest.c
@@ -7,12 +7,48 @@ typedef enum ers {OK, INV_FILE, INV_CAL, INV_VER, DUP_VER, INV_PRODID, DUP_PRODI
 
 
 ErrorCode parseCalendar (FILE * fp){
-    printf("In parseCalendar\n");
+    int checkVer = 0;
+    int checkID = 0;
     char buffer[75];
+    char * calVer;
+    char * calID;
     fgets(buffer, 75, fp);
-    printf("%s\n", buffer);
+    char * token = strtok(buffer, ":; \t");
+    while (1){
+        if (strcmp(token,"VERSION")==0){
+            if (checkVer == 0){
+                token = strtok(NULL, ":;\n");
+                calVer = malloc (sizeof(char)*strlen(token));
+                strcpy(calVer, token);
+                checkVer = 1;
+            } else {
+                free(calVer);
+                if (checkID == 1)
+                    free(calID);
+                return DUP_VER;
+            }
+        } else if (strcmp(token,"PRODID")==0){
+            if (checkID == 0){
+                token = strtok(NULL, ":;\n");
+                calID = malloc (sizeof(char)*strlen(token));
+                strcpy(calID, token);
+                checkID = 1;
+            } else {
+                free(calID);
+                if (checkVer == 1)
+                    free(calVer);
+                return DUP_PRODID;
+            }
+        } else if (strcmp(token,"BEGIN")==0){
 
+        } else if (strcmp(token,"COMMENT")==0){
 
+        } else {
+
+        }
+        fgets(buffer, 75, fp);
+        token = strtok(buffer, ":; \t");
+    }    
     return OK;
 }
 
@@ -44,7 +80,9 @@ ErrorCode createCalendar(char* fileName){
             }
         } else if (strcmp(token, "COMMENT")==0){
             printf("comment\n");
-        } 
+        } else {
+            return INV_CAL;
+        }
     }
     
 
