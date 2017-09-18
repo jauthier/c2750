4928846b3a6cc02a26ad213ed89093f0539b08bb
jauthier <jauthier@mail.uoguelph.ca>
2017-09-18 12:04:10 -0400

    a1

---
+++ A1/icTest.c
@@ -0,0 +1,25 @@
+
+
+ErrorCode createCalendar(char* fileName, Calendar** obj){
+    
+    //check that the file exists and open it
+    FILE * fp = fopen(fileName,"r");
+    if (fp == NULL){
+        return INV_FILE;
+    }
+    char buffer[75];
+
+    while (fgets(buffer,75,fp) != EOF){
+        printf("%s\n", buffer);
+
+        char * token = strtok(buffer, ":;");
+        printf("%s\n", token);
+
+        if (strcmp(token, "BEGIN")==0){
+            printf("found the start\n");
+        }
+    }
+
+
+    return 0;
+}
