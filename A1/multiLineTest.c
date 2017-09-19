297979822b2134fd5d2c5b0d079494258fc73166
jauthier <jauthier@mail.uoguelph.ca>
2017-09-19 12:36:15 -0400

    a1

--- A1/multiLineTest.c
+++ A1/multiLineTest.c
@@ -26,9 +26,93 @@ int checkMultiLine (char * firstLine, char * secondLine){
     return 0;
 }
 
+ErrorCode parseCalendar (FILE * fp){
+    /* Keeps track of whether or not there has been a 
+    version and product ID declared. There must be only
+    one version declared and only one product ID declared */
+    int checkVer = 0;
+    int checkID = 0;
+    /* hold the values of the version and porduct ID */
+    float calVer;
+    char * calID;
+
+    char current[75];
+    char * hold = fgets(current,75,fp);
+    char next[75];
+    int multi;
+    
+    while (hold != NULL){
+        /* if the line doesnt exist then it can't be a multi line */
+        hold = fgets(next,75,fp);
+        if (hold != NULL)
+            multi = checkMultiLine(current, next);
+        else
+            multi = 0;
+        /* parse the line */
+        char * token = strtok(current, ":; \t");
+        char * hold = strtok(NULL, ":;\n");
+        int len = strlen(hold) + 1;
+        char * value = malloc(sizeof(char)*len);
+        strcpy(value, hold);
+        while (multi == 1){
+            /* check if the line after the next line is also a multi line */
+            char buffer[75];
+            hold = fgets(buffer, 75, fp);
+            
+            if (hold != NULL)
+                multi = checkMultiLine(next, buffer);
+            else 
+                multi = 0;
+            /* realloc and add the next line to the end of value */
+            char * temp = strtok(next, "\n");
+            temp++;
+            len = len + strlen(temp);
+            value = realloc(value, len);
+            strcat(value, temp);
+            strcpy(next, buffer);
+        }
+
+        if (strcmp(token,"VERSION")==0){
+            if (checkVer == 0){ /* make sure this is the only declaration of the version */
+                calVer = atof(value);
+                checkVer = 1;
+            } else {
+                if (checkID == 1)
+                    free(calID);
+                return DUP_VER;
+            }
+        } else if (strcmp(token,"PRODID")==0){
+            if (checkID == 0){ /* make sure this is the only declaration of the product ID */
+                calID = malloc (sizeof(char)*strlen(value));
+                strcpy(calID, value);
+                checkID = 1;
+            } else {
+                free(calID);
+                return DUP_PRODID;
+            }
+        } else if (strcmp(token,"BEGIN")==0){
+            if (strcmp(value,"VCALENDAR")==0){
+                return INV_CAL;
+            }
+            if (checkID == 1 && checkVer == 1){
+                Event ** eventPrt = malloc(sizeof(Event*));
+                ErrorCode = ecode = parseEvent(fp, eventPrt);
+            }
+
+        } else if (strcmp(token,"COMMENT")==0){
+
+        } else if (strcmp(token,"END")==0){
+
+        } else {
+            return
+        }
+        free(value);
+        strcpy(current,next);
+    }    
+    return OK;
+}
 
 ErrorCode createCalendar(char* fileName){
-    
     /* check that the file exists and open it */
     FILE * fp = fopen(fileName,"r");
     if (fp == NULL){
@@ -71,11 +155,25 @@ ErrorCode createCalendar(char* fileName){
             strcpy(next, buffer);
         }
 
-        printf("%s:%s\n", token, value);
+        if (strcmp(token, "BEGIN")==0){
+            printf("%s\n", token);
+            /* if the next word is not VCALENDAR then the file is wrong
+            and INV_CAL is returned */
+            if (strcmp(value, "VCALENDAR") == 0){
+                ErrorCode eCode = parseCalendar(fp);
+                return eCode;
+            } else {
+                return INV_CAL;
+            }
+        } else if (strcmp(token, "COMMENT")==0){
+            continue;
+        } else {
+            return INV_CAL;
+        }
+        free(value);
         strcpy(current,next);
     }
-    
-    return OK;
+    return INV_CAL;
 }
 
 
