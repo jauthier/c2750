46d74adfe2aa41d44a2e4174944bdfb5c9561073
jauthier <jauthier@mail.uoguelph.ca>
2017-09-27 15:35:59 -0400

    a1

--- A1/testing.c
+++ A1/testing.c
@@ -175,7 +175,7 @@ void deleteEvent (Event * toDelete){
 
 char * printEvent(Event * toPrint){
 	char * uID = toPrint->UID;
-	char * dt = printDT(toPrint->creationDateTime);
+	char * dt = printDT(&toPrint->creationDateTime);
 	char * prop = toString(toPrint->properties);
 	char * alarm = toString(toPrint->alarms);
 	int len = strlen(uID) + strlen(dt) + strlen(prop) + strlen(alarm) + 5;
