8f9508c18c80fca58f8ee28c00fc05a573fa753a
jauthier <jauthier@mail.uoguelph.ca>
2017-10-01 21:17:09 -0400

    a1

--- A1/testing.c
+++ A1/testing.c
@@ -81,9 +81,8 @@ Alarm * initAlarm(char * action, char * trigger, List propList){
 
 char * printAlarm(void * toBePrinted){
     Alarm * toPrint = (Alarm *)toBePrinted;
-
     char * list = toString(toPrint->properties);
-    int len = strlen(list)+strlen(toPrint->action)+strlen(toPrint->trigger)+10;
+    int len = strlen(list)+200+strlen(toPrint->trigger)+8;
     char * str = malloc(sizeof(char)*len);
     sprintf(str,"%s\n%s\n%s\n", toPrint->action,toPrint->trigger,list);
     free(list);
