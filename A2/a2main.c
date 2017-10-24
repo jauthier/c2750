5b857a74bdedc6ac4c69186025c1a0b205b46eff
jauthier <jauthier@mail.uoguelph.ca>
2017-10-24 14:51:22 -0400

    a2

--- A2/a2main.c
+++ A2/a2main.c
@@ -7,26 +7,6 @@
 #include "LinkedListAPI.h"
 */
 
-void mainScr(WINDOW * win){
-
-	wmove(win, 4, 4);
-	waddstr(win, "1. Read iCalendar File");
-	wrefresh(win);
-	
-	wmove(win, 6, 4);
-	waddstr(win, "2. Display File");
-	wrefresh(win);
-
-	wmove(win, 8, 4);
-	waddstr(win, "3. Create iCalendar");
-	wrefresh(win);
-
-	wmove(win, 10, 4);
-	waddstr(win, "4. Exit");
-	wrefresh(win);
-}
-
-
 
 int main(int argc, char const *argv[]){
 
@@ -36,15 +16,13 @@ int main(int argc, char const *argv[]){
 	noecho();
 	keypad(stdscr, TRUE);
 
-	// create a new window
-	int nlines = COLS - 3;
-	int ncols = LINES - 3;
-	int y0 = 1;
-	int x0 = 1;
-	WINDOW * win = newwin(nlines, ncols, y0, x0);
-	mainScr(win);
+	mvprintw(4,2,"1. Read iCalendar File");
+	mvprintw(6, 2, "2. Display File");
+	mvprintw(8, 2, "3. Create iCalendar");
+	mvprintw(10, 2, "4. Exit");
+	
+
 	getch();
-	delwin(win);
 	endwin();
 	return 0;
 }
