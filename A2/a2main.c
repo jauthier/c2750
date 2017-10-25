8a95f62e68dc43b6478097611867b6a829beb2a6
jauthier <jauthier@mail.uoguelph.ca>
2017-10-25 14:21:19 -0400

    a2

--- A2/a2main.c
+++ A2/a2main.c
@@ -7,18 +7,7 @@
 #include "LinkedListAPI.h"
 */
 
-
-int main(int argc, char const *argv[]){
-
-	//ncurses initalization
-	initscr();
-	cbreak();
-	noecho();
-	keypad(stdscr, TRUE);
-
-	int yMax, xMax;
-	getmaxyx(stdscr, yMax, xMax);
-	
+int mainMenu(int yMax, int xMax){
 	// create a menu window
 	WINDOW * menuWin = newwin(1, xMax - 6, yMax - 4, 1);
 	refresh();
@@ -36,13 +25,12 @@ int main(int argc, char const *argv[]){
 	menu[3] = malloc(sizeof(char)*7);
 	strcpy(menu[3],"4. Exit");
 
-
 	while (1){
 
 		for (int i=0;i<4;i++){
 			if (highlight == i)
 				wattron(menuWin, A_REVERSE);
-			wmvprintw(i+2,2,"%s",menu[i]);
+			wmvprintw(i+1,2,"%s",menu[i]);
 			wattroff(menuWin, A_REVERSE);
 			
 		}
@@ -66,6 +54,150 @@ int main(int argc, char const *argv[]){
 
 	}
 
+	for (int j=0;j<4;j++){
+		free(menu[j]);
+	}
+	free(menu);
+	delWin(menuWin);
+	return highlight;
+}
+
+int readICalFIle(int yMax, int xMax){
+
+	WINDOW * readCalWin = newwin(1, xMax - 6, yMax - 4, 1);
+	refresh();
+	wrefresh(readCalWin);
+
+	//
+	wmvprintw(2,2,"iCalendar Create");
+	wmvprintw(4,2,"Product ID:");
+	wmvprintw(6,2,"Version:");
+	wmvprintw(8,2,"Event");
+	wmvprintw(10,4,"UID:");
+	wmvprintw(12,4,"Date-Time:");
+	wmvprintw(14,4,"Alarm");
+	wmvprintw(16,6,"Trigger:");
+	wmvprintw(18,6,"Action:");
+
+	/* ----- Product ID ----- */
+	wmove(readCalWin, 4, 15);
+	refresh()
+	wrefresh(readCalWin);
+	char buffer[200];
+	int i = 0;
+	echo();
+	int c = wgetch(readCalWin);
+	while(c != '\n'){
+		buffer[i] = c;
+		i++;
+		c = wgetch(readCalWin);
+	}
+	noecho();
+	buffer[i] = '\0';
+	// save the input
+	char * prodId = malloc(sizeof(char)*(strlen(buffer)+1));
+	strcpy(prodId, buffer);
+
+	/* ----- Version ----- */
+	wmove(readCalWin, 6, 12);
+	refresh()
+	wrefresh(readCalWin);
+	i = 0;
+	echo();
+	c = wgetch(readCalWin);
+	while(c != '\n'){
+		buffer[i] = c;
+		i++;
+		c = wgetch(readCalWin);
+	}
+	noecho();
+	buffer[i] = '\0';
+	// save the input
+	char * version = malloc(sizeof(char)*(strlen(buffer)+1));
+	strcpy(version, buffer);
+
+	/* ----- UID ----- */
+	wmove(readCalWin, 10, 12);
+	refresh()
+	wrefresh(readCalWin);
+	i = 0;
+	echo();
+	c = wgetch(readCalWin);
+	while(c != '\n'){
+		buffer[i] = c;
+		i++;
+		c = wgetch(readCalWin);
+	}
+	noecho();
+	buffer[i] = '\0';
+	// save the input
+	char * prodId = malloc(sizeof(char)*(strlen(buffer)+1));
+	strcpy(prodId, buffer);
+
+	delWin(readCalWin);
+	return 0;
+}
+
+int displayCal(int yMax, int xMax){
+
+	WINDOW * displayWin = newwin(1, xMax - 6, yMax - 4, 1);
+	refresh();
+	wrefresh(displayWin);
+
+	delWin(displayWin);
+	return 0;
+}
+
+int makeCal(int yMax, int xMax){
+
+	WINDOW * makeCalWin = newwin(1, xMax - 6, yMax - 4, 1);
+	refresh();
+	wrefresh(makeCalWin);
+
+	delWin(makeCalWin);
+	return 0;
+}
+
+int errScr(int yMax, int xMax){
+
+	WINDOW * errWin = newwin(1, xMax - 6, yMax - 4, 1);
+	refresh();
+	wrefresh(errWin);
+
+	delWin(errWin);
+	return 0;
+}
+
+int main(int argc, char const *argv[]){
+
+	//ncurses initalization
+	initscr();
+	cbreak();
+	noecho();
+	keypad(stdscr, TRUE);
+
+	int yMax, xMax;
+	getmaxyx(stdscr, yMax, xMax);
+	
+	while (1){
+
+		// start in main menu
+		int choice = mainMenu(yMax, xMax);
+
+		if (choice == 1){
+			// read file
+			readICalFIle(yMax, xMax);
+		} else if (choice == 2){
+			// Display cal using printCalendar
+			displayCal(yMax, xMax);
+		} else if (choice == 3){
+			// create cal
+			makeCal(yMax, xMax);
+		} else if (choice == 4){
+			// exit
+			break;
+		}
+	}
 
 	getch();
 	endwin();
