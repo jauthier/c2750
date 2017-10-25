4225a62b929aebd37ca5a4a5742cd0f23326951e
jauthier <jauthier@mail.uoguelph.ca>
2017-10-25 19:51:08 -0400

    a2

--- A2/a2main.c
+++ A2/a2main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curses.h>
+#include <unistd.h>
 
 #include "BasicFunctions.h"
 
@@ -9,8 +10,8 @@ int mainMenu(int yMax, int xMax);
 int makeCal(int yMax, int xMax);
 Calendar * calInit(char * prodId,  char * version, char * uid, char * dt, char * action, char * trigger);
 int saveCalendar(Calendar * cal);
-int displayCal(int yMax, int xMax);
-int readICalFIle(int yMax, int xMax);
+int displayCal(Calendar * obj);
+Calendar * readICalFIle(int yMax, int xMax);
 void errScr(char * msg);
 
 int mainMenu(int yMax, int xMax){
@@ -21,20 +22,22 @@ int mainMenu(int yMax, int xMax){
 
 	int choice;
 	int highlight = 0;
-	char ** menu = malloc(sizeof(char *)*4);
+	char ** menu = malloc(sizeof(char *)*5);
 	menu[0] = malloc(sizeof(char)*23);
 	strcpy(menu[0], "1. Read iCalendar File");
 	menu[1] = malloc(sizeof(char)*16);
 	strcpy(menu[1],"2. Display File");
 	menu[2] = malloc(sizeof(char)*20);
 	strcpy(menu[2],"3. Create iCalendar");
-	menu[3] = malloc(sizeof(char)*7);
-	strcpy(menu[3],"4. Exit");
+	menu[3] = malloc(sizeof(char)*35);
+	strcpy(menu[3],"4. Save iCalendar Object to a FIle");
+	menu[4] = malloc(sizeof(char)*7);
+	strcpy(menu[4],"5. Exit");
 
 	keypad(menuWin,TRUE);
 	while (1){
 
-		for (int i=0;i<4;i++){
+		for (int i=0;i<5;i++){
 			if (highlight == i)
 				wattron(menuWin, A_REVERSE);
 			mvwprintw(menuWin,i+2,2,"%s",menu[i]);
@@ -49,7 +52,7 @@ int mainMenu(int yMax, int xMax){
 					highlight --;
 				break;
 			case KEY_DOWN:
-				if (highlight+1 < 4)
+				if (highlight+1 < 5)
 					highlight++;
 				break;
 			default:
@@ -57,10 +60,8 @@ int mainMenu(int yMax, int xMax){
 		}
 		if (choice == 10)
 			break;
-
 	}
-
-	for (int j=0;j<4;j++){
+	for (int j=0;j<5;j++){
 		free(menu[j]);
 	}
 	
@@ -257,83 +258,288 @@ Calendar * calInit(char * prodId,  char * version, char * uid, char * dt, char *
 }
 
 int saveCalendar(Calendar * cal){
-	// create a menu window
+	/* make sure there is a calendar to save */
+	if (obj == NULL){
+		errScr("Calendar was not Saved: You have not entered a calendar object to save!")
+		return 0;
+	}
+
+	int save = 0;
 	int yMax, xMax;
 	getmaxyx(stdscr, yMax, xMax);
-	WINDOW * saveWin = newwin(yMax - 1, xMax - 1, 0, 0);
-	refresh();
-	wrefresh(saveWin);
-
-	int choice;
-	int highlight = 0;
-	char ** menu = malloc(sizeof(char *)*2);
-	menu[0] = malloc(sizeof(char)*26);
-	strcpy(menu[0], "1. Save iCalendar to File");
-	menu[1] = malloc(sizeof(char)*21);
-	strcpy(menu[1],"2. Exit to Main Menu");
-
-	mvwprintw(saveWin,1,1,"Calendar was made! If you do not save the Calendar it will be deleted.");
-
-	keypad(saveWin,TRUE);
+	char * fileName;
 	while (1){
-		for (int i=0;i<2;i++){
-			if (highlight == i)
-				wattron(saveWin, A_REVERSE);
-			mvwprintw(saveWin,i+2,2,"%s",menu[i]);
-			wattroff(saveWin, A_REVERSE);
-			
+		WINDOW * saveWin = newwin(yMax - 1, xMax - 1, 0, 0);
+		refresh();
+		wrefresh(saveWin);
+
+		mvwprintw(saveWin,1,1,"Enter the name of the file:");
+		// get user input
+		wmove(saveWin, 2, 1);
+		refresh();
+		wrefresh(saveWin);
+		char buffer[200];
+		int i = 0;
+		echo();
+		int c = wgetch(saveWin);
+		while(c != '\n'){
+			buffer[i] = c;
+			i++;
+			c = wgetch(saveWin);
 		}
-		choice = wgetch(saveWin);
+		noecho();
+		buffer[i] = '\0';
+		// save the input
+		fileName = malloc(sizeof(char)*(strlen(buffer)+1));
+		strcpy(fileName, buffer);
 
-		switch(choice){
-			case KEY_UP:
-				if (highlight-1 > -1)
-					highlight --;
-				break;
-			case KEY_DOWN:
-				if (highlight+1 < 2)
-					highlight++;
-				break;
-			default:
-				break;
-		}
-		if (choice == 10)
-			break;
-
-	}
-
-
-	if (highlight == 0){
-		//save the cal to a file
-	}
-
-	for (int j=0;j<2;j++){
-		free(menu[j]);
-	}
-	free(menu);
+		delwin(saveWin);
 	
-	delwin(saveWin);
-	return highlight;
-}
+		//see if the file already exists
+		if (access(fileName, F_OK) == 0){ // the file exists
+			// ask if its okay to replace the file
+			WINDOW * askWin = newwin(yMax - 1, xMax - 1, 0, 0);
+			refresh();
+			wrefresh(askWin);
+			mvwprintw(askWin, 1,1,"The file you entered already exists, would you like to replace it?");
+			int choice;
+			int highlight = 0;
+			char ** menu = malloc(sizeof(char *)*2);
+			menu[0] = malloc(sizeof(char)*4);
+			strcpy(menu[0], "Yes");
+			menu[1] = malloc(sizeof(char)*3);
+			strcpy(menu[1],"No");
+			keypad(askWin,TRUE);
+			while (1){
+				for (int i=0;i<2;i++){
+					if (highlight == i)
+						wattron(askWin, A_REVERSE);
+					mvwprintw(askWin,i+2,2,"%s",menu[i]);
+					wattroff(askWin, A_REVERSE);
+				}
+				choice = wgetch(askWin);
+				switch(choice){
+					case KEY_UP:
+						if (highlight-1 > -1)
+							highlight --;
+						break;
+					case KEY_DOWN:
+						if (highlight+1 < 2)
+							highlight++;
+						break;
+					default:
+						break;
+				}
+				if (choice == 10)
+					break;
+			}
 
-int displayCal(int yMax, int xMax){
+			for (int j=0;j<2;j++){
+				free(menu[j]);
+			}
+			free(menu);
+			delwin(askWin);
 
-	WINDOW * displayWin = newwin(yMax - 1, xMax - 1, 0, 0);
-	refresh();
-	wrefresh(displayWin);
+			if (highlight == 0){
+				save = 1;
+				break;
+			}
 
-	delwin(displayWin);
+			// ask if user wants to enter a new file name
+			WINDOW * againWin = newwin(yMax - 1, xMax - 1, 0, 0);
+			refresh();
+			wrefresh(againWin);
+			mvwprintw(againWin, 1,1,"Would you like to try a new file name?");
+			int choice;
+			int highlight = 0;
+			char ** menu = malloc(sizeof(char *)*2);
+			menu[0] = malloc(sizeof(char)*4);
+			strcpy(menu[0], "Yes");
+			menu[1] = malloc(sizeof(char)*3);
+			strcpy(menu[1],"No");
+			keypad(againWin,TRUE);
+			while (1){
+				for (int i=0;i<2;i++){
+					if (highlight == i)
+						wattron(againWin, A_REVERSE);
+					mvwprintw(againWin,i+2,2,"%s",menu[i]);
+					wattroff(againWin, A_REVERSE);
+				}
+				choice = wgetch(againWin);
+				switch(choice){
+					case KEY_UP:
+						if (highlight-1 > -1)
+							highlight --;
+						break;
+					case KEY_DOWN:
+						if (highlight+1 < 2)
+							highlight++;
+						break;
+					default:
+						break;
+				}
+				if (choice == 10)
+					break;
+			}
+
+			for (int j=0;j<2;j++){
+				free(menu[j]);
+			}
+			free(menu);
+			delwin(againWin);
+			if (highlight == 1){
+				break;
+			}
+		} else {
+			save = 1;
+			break;
+		}
+	}
+
+	if (save = 1){
+		ICalErrorCode ec = writeCalendar(fileName , obj);
+		if (ec == OK)
+			return 1;
+	}
 	return 0;
 }
 
-int readICalFIle(int yMax, int xMax){
+int displayCal(Calendar * obj){
 
-	WINDOW * makeCalWin = newwin(yMax - 1, xMax - 1, 0, 0);
-	refresh();
-	wrefresh(makeCalWin);
+	if ( obj == NULL){
+		errScr("Cannot Display Calendar: You must read in a calendar file before it can be displayed!")
+		return 0;
+	}
 
-	delwin(makeCalWin);
+	int yMax, xMax;
+	getmaxyx(stdscr, yMax, xMax);
+
+	char * toPrint = printCalendar(obj);
+	if (toPrint == NULL){
+		errScr("Cannot Display Calendar: There was an error reading the calendar.");
+	} else {
+		WINDOW * displayWin = newwin(yMax - 1, xMax - 1, 0, 1);
+		refresh();
+		wrefresh(displayWin);
+		mvwprintw(displayWin, 1, 1, "%s",toPrint);
+		delwin(displayWin);
+	}
 	return 0;
+}
+
+Calendar * readICalFIle(int yMax, int xMax){
+	Calendar * obj;
+	while (1){
+
+		WINDOW * calFileWin = newwin(yMax - 1, xMax - 1, 0, 0);
+		refresh();
+		wrefresh(calFileWin);
+
+		mvwprintw(calFileWin,1,1,"Enter the name of the file:");
+		// get user input
+		wmove(calFileWin, 2, 1);
+		refresh();
+		wrefresh(calFileWin);
+		char buffer[200];
+		int i = 0;
+		echo();
+		int c = wgetch(calFileWin);
+		while(c != '\n'){
+			buffer[i] = c;
+			i++;
+			c = wgetch(calFileWin);
+		}
+		noecho();
+		buffer[i] = '\0';
+		// save the input
+		char * fileName = malloc(sizeof(char)*(strlen(buffer)+1));
+		strcpy(fileName, buffer);
+		delwin(calFileWin);
+
+		ICalErrorCode ec = createCalendar(fileName, &obj);
+		readError(ec);
+		if (ec == INV_FILE){
+			// ask if user wants to enter a new file name
+			WINDOW * askWin = newwin(yMax - 1, xMax - 1, 0, 0);
+			refresh();
+			wrefresh(askWin);
+			mvwprintw(askWin, 1,1,"Would you like to try a new file name?");
+			int choice;
+			int highlight = 0;
+			char ** menu = malloc(sizeof(char *)*2);
+			menu[0] = malloc(sizeof(char)*4);
+			strcpy(menu[0], "Yes");
+			menu[1] = malloc(sizeof(char)*3);
+			strcpy(menu[1],"No");
+			keypad(askWin,TRUE);
+			while (1){
+				for (int i=0;i<2;i++){
+					if (highlight == i)
+						wattron(askWin, A_REVERSE);
+					mvwprintw(askWin,i+2,2,"%s",menu[i]);
+					wattroff(askWin, A_REVERSE);
+				}
+				choice = wgetch(askWin);
+				switch(choice){
+					case KEY_UP:
+						if (highlight-1 > -1)
+							highlight --;
+						break;
+					case KEY_DOWN:
+						if (highlight+1 < 2)
+							highlight++;
+						break;
+					default:
+						break;
+				}
+				if (choice == 10)
+					break;
+			}
+
+			for (int j=0;j<2;j++){
+				free(menu[j]);
+			}
+			free(menu);
+			delwin(askWin);
+			if (highlight == 1){
+				return NULL;
+			}
+
+		} else {
+			if (ec == OK)
+				return obj;
+			else 
+				return NULL;
+		}
+	}
+	return NULL;
+}
+
+void readError(ICalErrorCode err){
+	if (err == INV_CAL)
+        errScr("Calendar not Created: Invaid Calendar");
+    else if (err == OK)
+        errScr("The calendar was successfully created!");
+    else if (err == INV_FILE)
+        errScr("Calendar not Created: Invaid File");
+    else if (err == INV_VER)
+        errScr("Calendar not Created: Invaid Version");
+    else if (err == INV_PRODID)
+        errScr("Calendar not Created: Invaid Product ID");
+    else if (err == INV_EVENT)
+        errScr("Calendar not Created: Invaid Event");
+    else if (err == DUP_VER)
+        errScr("Calendar not Created: Duplicate Version");
+    else if (err == DUP_PRODID)
+        errScr("Calendar not Created: Duplicate Product ID");
+    else if (err == INV_CREATEDT)
+        errScr("Calendar not Created: Invaid Date-Time");
+    else if (err == INV_ALARM)
+        errScr("Calendar not Created: Invaid Alarm");
+    else if (err == WRITE_ERROR)
+        errScr("Calendar not Created: Write Error");
+    else
+        errScr("Calendar not Created: Other Error");
 }
 
 void errScr(char * msg){
@@ -379,10 +585,13 @@ int main(int argc, char const *argv[]){
 			// create cal
 			makeCal(yMax, xMax);
 		} else if (choice == 3){
+			// save calendar
+		} else if (choice == 4){
 			// exit
 			endwin();
 			return 0;
 		}
+
 	}
 
 	getch();
